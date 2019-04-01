7967a7348617faabc09705b2218718a1976fb205
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-04-01 15:24:22 -0700

    finally got around CORS :)

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-function Location(props) {
-  return <div key={props.location}>{props.location}</div>;
+class Location extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      loading: false,
+      data: {
+        summary: null
+      }
+    };
+  }
+  componentDidMount() {
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api =
+      "https://api.darksky.net/forecast/1b3d30bceaa757a136e3c1dfad9801d8/";
+    fetch(proxy + api + this.props.location)
+      .then(response => response.json())
+      .then(data => this.setState({ data: { summary: data.hourly.summary } }));
+  }
+  render() {
+    const { data } = this.state;
+    return <div key={this.props.location}>{data.summary}</div>;
+  }
 }
 
 export default Location;
