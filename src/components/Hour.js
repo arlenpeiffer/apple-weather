7443be65b131f149af3acdb4fa473355ea1d4c2f
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-04-09 21:39:56 -0700

    added some css to the Hours and Hour components

--- src/components/Hour.js
+++ src/components/Hour.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./Hour.css";
 
 function Hour(props) {
   return (
-    <div>
-      <p>
-        {props.hour.time} - {props.hour.temp}°
-      </p>
+    <div className="hour">
+      <p>{props.hour.time}</p>
+      <p>{props.hour.icon}</p>
+      <p>{props.hour.temp}°</p>
     </div>
   );
 }
--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Hour from "./Hour";
+import "./Hours.css";
 
 function Hours(props) {
   let hours24 = props.hours.slice(0, 24);
 
   return (
-    <div>
+    <div className="hours">
       {hours24.map(hour => (
         <Hour key={hour.index} hour={hour} />
       ))}
