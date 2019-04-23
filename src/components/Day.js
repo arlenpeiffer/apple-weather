1f5007b9d3c3cdcfe367096b1fa323a9a83aaa82
Arlen Peiffer <arlenpeiffer@gmail.com>
2019-04-22 22:50:56 -0700

    for consistency's sake..

--- src/components/Day.js
+++ src/components/Day.js
@@ -5,12 +5,11 @@ import "../weather-icons/css/weather-icons.css";
 
 function Day(props) {
   const { high, icon, low, weekday } = props.day;
-  const darkSkyPrefix = "wi wi-forecast-io-";
 
   return (
     <div className="row">
       <p>{weekday}</p>
-      <i className={darkSkyPrefix + icon} />
+      <i className={"wi wi-forecast-io-" + icon} />
       <p>High: {Math.round(high) + "°"}</p>
       <p>Low: {Math.round(low) + "°"}</p>
     </div>
